1146e6259d88af745036ac0a7534523acc055698
Abhiram Kothapalli <abhiramkothapalli@Abhirams-MacBook-Pro.local>
2017-01-03 00:18:13 -0800

    Finished tentative screener

--- screener.py
+++ screener.py
@@ -22,39 +22,93 @@ def readfile(filename):
 
 
 def highest_high(d, ticker, start, days):
-    return max([x[0] for x in d[ticker][start:start+days]])
+    if start < days - 1:
+        raise IndexError("Highest high start is out of bounds")
+    return max([float(x[0]) for x in d[ticker][start - days:start + 1]])
 
 def lowest_low(d, ticker, start, days):
-    return min([x[1] for x in d[ticker][start:start+days]])
+    if start < days - 1:
+        raise IndexError("Lowest low start is out of bounds")
+    return min([float(x[1]) for x in d[ticker][start - days:start + 1]])
 
 def chikou(d, ticker, start, days=26):
-    if start < days:
-        raise IndexError("Chikou needs start > days")
-    return d[ticker][start - days][2]
+    if start > len(d[ticker]) - days:
+        raise IndexError("Chikou start is out of bounds")
+    return d[ticker][start + days][2]
 
 def avg(hh, ll):
     return (hh + ll) / 2
 
-def is_valid(d, ticker):
-    hh9 = highest_high(d, ticker, )
-    ll9 =
-    hh26 =
-    ll26 =
-    hh52 =
-    ll52 = 
-    tenkansen = 
+
+def is_valid(d, ticker, days=26):
+
+    if len(d[ticker]) != 84:
+        return False
+    
+    mainday = len(d[ticker]) - days - 1
+    
+    hh9 = highest_high(d, ticker, mainday, 9)
+    ll9 = lowest_low(d, ticker, mainday, 9)
+    hh26 = highest_high(d, ticker, mainday, 26)
+    ll26 = lowest_low(d, ticker, mainday, 26)
+    hh52 = highest_high(d, ticker, mainday, 52)
+    ll52 = lowest_low(d, ticker, mainday, 52)
+    tenkansen = avg(hh9, ll9)
+    kijunsen = avg(hh26, ll26)
+    chikouspan = chikou(d, ticker, mainday)
+    prev_chikouspan = chikou(d, ticker, mainday - 1)
+    senkoua = avg(tenkansen, kijunsen)
+    senkoub = avg(hh52, ll52)
 
 
+    '''
+    print hh9
+    print ll9
+    print hh26
+    print ll26
+    print hh52
+    print ll52
+    print tenkansen
+    print kijunsen
+    print chikouspan
+    print senkoua
+    print senkoub
+    '''
 
+    close = d[ticker][mainday][2]
+    prev_close = d[ticker][mainday - 1][2]
+    
+    
+    if (close > senkoua and
+        close > senkoub and
+        chikouspan >= close and
+        prev_chikouspan < prev_close):
+        return True
 
+    if (close < senkoua and
+        close < senkoub and
+        chikouspan <= close and
+        prev_chikouspan > prev_close):
+        return True
 
+    return False
+        
+    
+        
+
+def get_valid_tickers(d):
+    valid_tickers = []
+    for ticker in d:
+        if is_valid(d, ticker):
+            valid_tickers.append(ticker)
+    return valid_tickers
+            
     
 
 
 
-
 d = readfile("sampledata/WIKI_PRICES_010158fe7c92b40ebea4a642c5d0b158.csv")
-print(chikou(d, "A", 27, 26))
+print(get_valid_tickers(d))
 
 
 
