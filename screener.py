90573febde5cdb1b5a2efe35ab9c5603c0a7bab7
Shyam Karthick Ravikumar <shyamkarthickravikumar@Shyams-MacBook-Pro.local>
2017-01-08 17:23:56 -0800

    extensive changes have been made, ready to be used legitly

--- screener.py
+++ screener.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import csv
 
 def readfile(filename):
@@ -33,27 +31,39 @@ def chikou(d, ticker, start, days=25):
 def avg(hh, ll):
     return float((hh + ll) / 2)
 
-
-def is_valid(d, ticker, days=26):
-    if len(d[ticker]) != 84:
+def is_signal_one(d, ticker, days=26):
+    if len(d[ticker]) != 111:
         return False
     
     mainday = len(d[ticker]) - days - 1
+
     hh9 = highest_high(d, ticker, mainday, 9)
     ll9 = lowest_low(d, ticker, mainday, 9)
+    hh9shift = highest_high(d, ticker, mainday-25, 9)
+    ll9shift = lowest_low(d, ticker, mainday-25, 9)
+
     hh26 = highest_high(d, ticker, mainday, 26)
     ll26 = lowest_low(d, ticker, mainday, 26)
-    hh52 = highest_high(d, ticker, mainday, 52)
-    ll52 = lowest_low(d, ticker, mainday, 52)
+    hh26shift = highest_high(d, ticker, mainday-25, 26)
+    ll26shift = lowest_low(d, ticker, mainday-25, 26)
+
+    hh52shift = highest_high(d, ticker, mainday-25, 52)
+    ll52shift = lowest_low(d, ticker, mainday-25, 52)
+
     tenkansen = avg(hh9, ll9)
+    tenkansenshift = avg(hh9shift, ll9shift)
+
     kijunsen = avg(hh26, ll26)
+    kijunsenshift = avg(hh26shift, ll26shift)
+
     chikouspan = chikou(d, ticker, mainday)
     prev_chikouspan = chikou(d, ticker, mainday - 1)
-    senkoua = avg(tenkansen, kijunsen)
-    senkoub = avg(hh52, ll52)
+
+    senkoua = avg(tenkansenshift, kijunsenshift)
+    senkoub = avg(hh52shift, ll52shift)
+
     close = float(d[ticker][mainday][2])
     prev_close = float(d[ticker][mainday - 1][2])
-    
     
     if (close > senkoua and
         close > senkoub and
@@ -66,22 +76,109 @@ def is_valid(d, ticker, days=26):
         prev_chikouspan > prev_close):
         return True
     return False
+
+def is_signal_two(d, ticker, days=0):
+    if len(d[ticker]) != 111:
+        return False
+    
+    mainday = len(d[ticker]) - days - 1
+
+    hh9 = highest_high(d, ticker, mainday, 9)
+    ll9 = lowest_low(d, ticker, mainday, 9)
+    hh9shift = highest_high(d, ticker, mainday-25, 9)
+    ll9shift = lowest_low(d, ticker, mainday-25, 9)
+    hh9prev = highest_high(d, ticker, mainday-1, 9)
+    ll9prev = lowest_low(d, ticker, mainday-1, 9)
+
+    hh26 = highest_high(d, ticker, mainday, 26)
+    ll26 = lowest_low(d, ticker, mainday, 26)
+    hh26shift = highest_high(d, ticker, mainday-25, 26)
+    ll26shift = lowest_low(d, ticker, mainday-25, 26)
+    hh26prev = highest_high(d, ticker, mainday-1, 26)
+    ll26prev = lowest_low(d, ticker, mainday-1, 26)
+
+    hh52shift = highest_high(d, ticker, mainday-25, 52)
+    ll52shift = lowest_low(d, ticker, mainday-25, 52)
+
+    tenkansen = avg(hh9, ll9)
+    tenkansenshift = avg(hh9shift, ll9shift)
+    prev_tenkansen = avg(hh9prev, ll9prev)
+
+    kijunsen = avg(hh26, ll26)
+    kijunsenshift = avg(hh26shift, ll26shift)
+    prev_kijunsen = avg(hh26prev, ll26prev)
+
+    senkoua = avg(tenkansenshift, kijunsenshift)
+    senkoub = avg(hh52shift, ll52shift)
+    
+    close = float(d[ticker][mainday][2])
+    prev_close = float(d[ticker][mainday - 1][2])
+    
+    if (kijunsen > senkoua and
+        kijunsen > senkoub and
+        tenkansen >= kijunsen and
+        prev_tenkansen < prev_kijunsen):
+        return True
+    if (kijunsen < senkoua and
+        kijunsen < senkoub and
+        tenkansen <= kijunsen and
+        prev_tenkansen > prev_kijunsen):
+        return True
+    return False
         
-def get_valid_tickers(d):
+def get_signal_one(d):
     valid_tickers = []
     for ticker in d:
-        if is_valid(d, ticker):
+        if is_signal_one(d, ticker):
             valid_tickers.append(ticker)
     return valid_tickers
-            
+
+def get_signal_two(d):
+    valid_tickers = []
+    for ticker in d:
+        if is_signal_two(d, ticker):
+            valid_tickers.append(ticker)
+    return valid_tickers
+
+def get_combo(d):
+    valid_tickers = []
+    sig_one = get_signal_one(d)
+    sig_two = get_signal_two(d)
+
+    for ticker in sig_one:
+        if (is_signal_two(d, ticker, 0) or 
+            is_signal_two(d, ticker, 1) or 
+            is_signal_two(d, ticker, 2) or 
+            is_signal_two(d, ticker, 3) or 
+            is_signal_two(d, ticker, 4)):
+            valid_tickers.append(ticker)
+
+    for ticker in sig_two:
+        if (is_signal_one(d, ticker, 26) or 
+            is_signal_one(d, ticker, 27) or 
+            is_signal_one(d, ticker, 28) or 
+            is_signal_one(d, ticker, 29) or 
+            is_signal_one(d, ticker, 30)):
+            valid_tickers.append(ticker)
+
+    return valid_tickers
+
+def filter_price(d, tickers, min):
+    valid_tickers = []
+    for ticker in tickers:
+        price = float(d[ticker][len(d[ticker]) - 1][2])
+        if price >= min:
+            valid_tickers.append(ticker)
+    return valid_tickers
+
+def remove_duplicates(array):
+    valid_tickers = []
+    for ticker in array:
+        if ticker not in valid_tickers:
+            valid_tickers.append(ticker)
+    return valid_tickers
     
 d = readfile("datafortoday.csv")
-print(get_valid_tickers(d))
-
-
-
-
-        
-        
-    
-    
+print("Both signals: " + ', '.join(remove_duplicates(filter_price(d, get_combo(d), 10))) + '\n')
+print("Signal One (Chikou): " + ', '.join(filter_price(d, get_signal_one(d), 10)) + '\n')
+print("Signal Two (Tenkansen): " + ', '.join(filter_price(d, get_signal_two(d), 10)))
